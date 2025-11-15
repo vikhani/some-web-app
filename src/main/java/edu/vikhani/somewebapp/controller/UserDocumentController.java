68cca04e14bcf717764578d2cb315ac2ebca99f4
vikhani <vikhanieva@gmail.com>
2025-11-15 20:27:54 +0300

    Add checkstyle

--- src/main/java/edu/vikhani/somewebapp/controller/UserDocumentController.java
+++ src/main/java/edu/vikhani/somewebapp/controller/UserDocumentController.java
@@ -3,11 +3,13 @@ package edu.vikhani.somewebapp.controller;
 import edu.vikhani.somewebapp.model.UserDocument;
 import edu.vikhani.somewebapp.service.UserDocumentService;
 import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
 
+@Slf4j
 @RestController
 @RequestMapping("/api/documents")
 @RequiredArgsConstructor
@@ -17,6 +19,12 @@ public class UserDocumentController {
 
     @GetMapping("/{userName}")
     public ResponseEntity<List<UserDocument>> getDocuments(@PathVariable String userName) {
+        log.debug("1");
+        log.debug("2");
+        log.debug("3");
+        log.debug("4");
+        log.debug("5");
+        log.debug("6");
         return ResponseEntity.ok(service.getDocumentsForUser(userName));
     }
 
